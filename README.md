168b2e19c62eb316f9e9ac4a6edcf3e4f833c16b
NicoCroce <ingcrocenicolas@outlook.com>
2014-06-19 11:46:12 -0300

    Todo el proyecto.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-mini-blog
-=========
+mini-blog-node
+==============
---
+++ app/js/controllers/newPostCtrl.js
@@ -0,0 +1,21 @@
+'use strict';
+
+app.controller('newPostCtrl', function($scope, blogService, $location, toaster) {
+    // Call to blogService.create()
+    $scope.addPost = function() {
+        var postData = {
+            id : '',
+            title : $scope.titlePost,
+            text : $scope.bodyPost
+        };
+        blogService.create(postData)
+            .success(function (current, status, headers, config) {
+                debugger;
+                $location.path("/posts");
+                toaster.pop('success', "Post saved successfully!");
+            })
+            .error(function(current, status, headers, config) {
+                toaster.pop('error', current);
+            });
+    };
+});
